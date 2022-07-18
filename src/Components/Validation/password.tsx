6a23eddbee6ad7c4b9e63b628afd70a530897bf9
Cidrallll <lucas.woginiack@yahoo.com.br>
2022-07-17 22:33:51 -0300

    checking email and password on login

--- src/Components/Validation/password.tsx
+++ src/Components/Validation/password.tsx
@@ -1,14 +1,39 @@
 export default function ValidationPassword(password: string) {
-    
     let res = Validation(password);
     return res;
 }
 
 function Validation(password: string) {
 
-    if(password.length < 4) {
+    let smallLetters = false;
+    let capitalLetters = false;
+    let number = false;
+    
+    // verifica se tem letras minusculas
+    if(/[a-z]/gm.test(password)) {
+        smallLetters = true;
+    }
+
+    // verifica se tem letra maiusculas
+    if (/[A-Z]/gm.test(password)) {
+        capitalLetters = true;
+    }
+
+    // verifica se tem numeros
+    if (/[0-9]/gm.test(password)) {
+        number = true;
+        console.log('Numeros')
+    }
+
+    if (!smallLetters || !capitalLetters || !number) {
         return false;
-    }else {
-        return true;
+    }else if (smallLetters && capitalLetters && number) {
+        // verifica se tem pelo menos 6 caracteres
+        if(password.length < 6) {
+            return false;
+        }else {
+            return true;
+        }
+
     }
 }
